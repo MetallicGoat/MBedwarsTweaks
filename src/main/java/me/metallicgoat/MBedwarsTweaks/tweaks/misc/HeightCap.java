abb8a6031599d5ae92ca2b5a015260d7ce96d23e
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-09-18 12:39:50 -0400

    Add HeightCap Message

--- src/main/java/me/metallicgoat/MBedwarsTweaks/tweaks/misc/HeightCap.java
+++ src/main/java/me/metallicgoat/MBedwarsTweaks/tweaks/misc/HeightCap.java
@@ -3,6 +3,7 @@ package me.metallicgoat.MBedwarsTweaks.tweaks.misc;
 import de.marcely.bedwars.api.BedwarsAPI;
 import de.marcely.bedwars.api.arena.Arena;
 import de.marcely.bedwars.api.arena.ArenaStatus;
+import de.marcely.bedwars.api.message.Message;
 import me.metallicgoat.MBedwarsTweaks.utils.ServerManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -23,6 +24,10 @@ public class HeightCap implements Listener {
                     String[] token = s.split(":");
                     if (arena.getName().equalsIgnoreCase(token[0])) {
                         if (e.getBlockPlaced().getY() > Integer.parseInt(token[1])) {
+                            String message = ServerManager.getConfig().getString("Height-Cap.Message");
+                            if(message != null && !message.equals("")) {
+                                player.sendMessage(Message.build(message).done());
+                            }
                             e.setCancelled(true);
                         }
                     }
