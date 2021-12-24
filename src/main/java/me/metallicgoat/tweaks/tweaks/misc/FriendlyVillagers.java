b78b19592cd7dbee12518f0d7ffe03f9aebde8f2
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-12-24 01:12:10 -0500

    Finally make Friendly Villagers work properly

--- src/main/java/me/metallicgoat/tweaks/tweaks/misc/FriendlyVillagers.java
+++ src/main/java/me/metallicgoat/tweaks/tweaks/misc/FriendlyVillagers.java
@@ -20,45 +20,53 @@ import java.util.*;
 
 public class FriendlyVillagers implements Listener {
 
-    private static final MBedwarsTweaks plugin = MBedwarsTweaks.getInstance();
+    private final MBedwarsTweaks plugin = MBedwarsTweaks.getInstance();
     private BukkitTask task;
     private final List<World> worlds = new ArrayList<>();
     private boolean isRunning = false;
 
     @EventHandler
     public void onRoundStart(RoundStartEvent e){
-        if(ServerManager.getConfig().getBoolean("Friendly-Villagers")) {
-            World world = e.getArena().getGameWorld();
-            if (world != null && !worlds.contains(world)) {
-                worlds.add(world);
-            }
-            if (!isRunning && !worlds.isEmpty()) {
-                startLooking();
-                isRunning = true;
-            }
+        if(!ServerManager.getConfig().getBoolean("Friendly-Villagers"))
+            return;
+
+        final World world = e.getArena().getGameWorld();
+
+        if (world != null && !worlds.contains(world))
+            worlds.add(world);
+
+        if (!isRunning && !worlds.isEmpty()) {
+            startLooking();
+            isRunning = true;
         }
     }
 
     @EventHandler
     public void onRoundEnd(RoundEndEvent e){
         World world = e.getArena().getGameWorld();
-        if(world != null) {
-            WorldStorage worldStorage = BedwarsAPI.getWorldStorage(world);
-            if(worldStorage != null) {
-                worldStorage.getHolograms().forEach(hologramEntity -> {
-                    if (hologramEntity.getControllerType() == HologramControllerType.DEALER
-                            || hologramEntity.getControllerType() == HologramControllerType.UPGRADE_DEALER) {
-                        hologramEntity.teleport(hologramEntity.getSpawnLocation());
-                    }
-                });
-                if (ServerManager.getConfig().getBoolean("Friendly-Villagers")) {
-                    worlds.remove(world);
-                if (worlds.isEmpty() && task != null) {
-                        task.cancel();
-                        isRunning = false;
-                    }
-                }
+        if(world == null)
+            return;
+
+        WorldStorage worldStorage = BedwarsAPI.getWorldStorage(world);
+
+        if(worldStorage == null)
+            return;
+
+        worldStorage.getHolograms().forEach(hologramEntity -> {
+            if (hologramEntity.getControllerType() == HologramControllerType.DEALER
+                    || hologramEntity.getControllerType() == HologramControllerType.UPGRADE_DEALER) {
+                hologramEntity.teleport(hologramEntity.getSpawnLocation());
             }
+        });
+
+        if (!ServerManager.getConfig().getBoolean("Friendly-Villagers"))
+            return;
+
+        worlds.remove(world);
+
+        if (worlds.isEmpty() && task != null) {
+            task.cancel();
+            isRunning = false;
         }
     }
 
@@ -70,45 +78,70 @@ public class FriendlyVillagers implements Listener {
 
             WorldStorage worldStorage = BedwarsAPI.getWorldStorage(world);
 
-            if (worldStorage != null) {
+            if (worldStorage == null)
+                return;
 
-                //Get all villagers in the world
-                Collection<HologramEntity> entity = worldStorage.getHolograms();
+            //Get all villagers in the world
+            Collection<HologramEntity> entity = worldStorage.getHolograms();
 
-                //For each villager
-                entity.forEach(hologramEntity -> {
+            //For each villager
+            entity.forEach(hologramEntity -> {
 
-                    if (hologramEntity.getControllerType() == HologramControllerType.DEALER
-                            || hologramEntity.getControllerType() == HologramControllerType.UPGRADE_DEALER) {
+                if (hologramEntity.getControllerType() == HologramControllerType.DEALER
+                        || hologramEntity.getControllerType() == HologramControllerType.UPGRADE_DEALER) {
 
-                        //Get players in range of villager
-                        Player[] playersArray = hologramEntity.getSeeingPlayers();
+                    //Get players in range of villager
+                    Player[] playersArray = hologramEntity.getSeeingPlayers();
 
-                        if (playersArray.length > 0) {
-                            //Get the closest player
-                            Player lookAtPlayer = Arrays.stream(playersArray).min(Comparator.comparingDouble(p -> p.getLocation().distanceSquared(hologramEntity.getLocation()))).get();
+                    if (playersArray.length > 0) {
+                        //Get the closest player
+                        Player lookAtPlayer = Arrays.stream(playersArray).min(Comparator.comparingDouble(p -> p.getLocation().distanceSquared(hologramEntity.getLocation()))).get();
 
-                            //Final location
-                            Location moveTo = hologramEntity.getLocation().setDirection(lookAtPlayer.getLocation().subtract(hologramEntity.getLocation()).toVector());
+                        //Final location
+                        Location moveTo = hologramEntity.getLocation().setDirection(lookAtPlayer.getLocation().subtract(hologramEntity.getLocation()).toVector());
 
-                            //Smooth Look (Interpolation)
-                            //Time wasted so far: 2.5 fucking hours
-                            //TODO Still does not really work if there are 2 players
-                            float currentYaw = hologramEntity.getLocation().getYaw();
-                            float targetYaw = moveTo.getYaw();
-                            float newYaw;
+                        //Smooth Look (Interpolation)
+                        //Time wasted so far: 4 fucking hours
+                        float currentYaw = hologramEntity.getLocation().getYaw();
+                        float targetYaw = moveTo.getYaw();
 
-                            if ((Math.abs(currentYaw) + Math.abs(targetYaw)) > 353)
-                                newYaw = targetYaw;
-                            else
-                                newYaw = currentYaw + (targetYaw - currentYaw) / 4;
-
-                            moveTo.setYaw(newYaw);
-                            hologramEntity.teleport(moveTo);
-                        }
+                        hologramEntity.teleport(calculateMove(moveTo, currentYaw, targetYaw));
                     }
-                });
-            }
+                }
+            });
         }), 0L, 1);
+    }
+
+    private static Location calculateMove(Location moveTo, float currentYaw, float targetYaw){
+        while (targetYaw >= 180F) {
+            targetYaw -= 360F;
+        }
+        while (targetYaw < -180F) {
+            targetYaw += 360F;
+        }
+        double adjusted = targetYaw - 40;
+        while (adjusted >= 180F) {
+            adjusted -= 360F;
+        }
+        while (adjusted < -180F) {
+            adjusted += 360F;
+        }
+        if (adjusted > currentYaw) {
+            moveTo.setYaw((float) adjusted);
+        }
+        if (adjusted != currentYaw) {
+            adjusted = targetYaw + 40;
+            while (adjusted >= 180F) {
+                adjusted -= 360F;
+            }
+            while (adjusted < -180F) {
+                adjusted += 360F;
+            }
+            if (adjusted < currentYaw) {
+                moveTo.setYaw((float) adjusted);
+            }
+        }
+        moveTo.setYaw(targetYaw);
+        return moveTo;
     }
 }
