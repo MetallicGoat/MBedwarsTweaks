84aca9b1c13c28b291a100d00df9418541715f06
MetallicGoat <the.great.metallic.goat@gmail.com>
2023-03-26 13:15:33 -0400

    Improve Personal EnderChests

--- src/main/java/me/metallicgoat/tweaksaddon/tweaks/misc/PersonalChests.java
+++ src/main/java/me/metallicgoat/tweaksaddon/tweaks/misc/PersonalChests.java
@@ -21,46 +21,44 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PersonalChests implements Listener {
 
-    private static final HashMap<Inventory, Arena> inventoryArenaHashMap = new HashMap<>();
+    private static final HashMap<Arena, List<Inventory>> inventoryArenaHashMap = new HashMap<>();
     private static final HashMap<Player, Block> openChests = new HashMap<>();
 
     @EventHandler
-    public void onRoundStart(RoundStartEvent event){
-        if(!ConfigValue.personal_ender_chests_enabled)
+    public void onRoundStart(RoundStartEvent event) {
+        if (!ConfigValue.personal_ender_chests_enabled)
             return;
 
         final Arena arena = event.getArena();
-        for(Player player : arena.getPlayers()){
+        final List<Inventory> inventories = new ArrayList<>();
+
+        for (Player player : arena.getPlayers()) {
             final Team team = arena.getPlayerTeam(player);
             final String teamName = team != null ? ChatColor.stripColor(team.getDisplayName()) : "";
-            final String teamColor = team != null ? "&" + team.getChatColor().getChar() : "";
+            final String teamColor = team != null ? "" + team.getBungeeChatColor() : "";
             final String chestName = Message.build(ConfigValue.personal_ender_chests_name)
                     .placeholder("team-name", teamName)
                     .placeholder("team-color", teamColor)
                     .done();
 
-            final Inventory inventory = Bukkit.createInventory(player, 27, chestName);
-            inventoryArenaHashMap.put(inventory, arena);
+            inventories.add(Bukkit.createInventory(player, 27, chestName));
         }
+
+        inventoryArenaHashMap.put(arena, inventories);
     }
 
     @EventHandler
-    public void onRoundEnd(RoundEndEvent e){
-        inventoryArenaHashMap.values().removeAll(Collections.singleton(e.getArena()));
+    public void onRoundEnd(RoundEndEvent event) {
+        inventoryArenaHashMap.remove(event.getArena());
     }
 
-    // TODO Do we need to cancel this event?
-
     @EventHandler(priority = EventPriority.HIGH)
-    public void onChestOpen(PlayerInteractEvent event){
-
-        if(!ConfigValue.personal_ender_chests_enabled)
+    public void onChestOpen(PlayerInteractEvent event) {
+        if (!ConfigValue.personal_ender_chests_enabled)
             return;
 
         final Player player = event.getPlayer();
@@ -69,29 +67,31 @@ public class PersonalChests implements Listener {
 
         // Check if player is opening chest in an arena
         if (arena == null || block == null ||
+                block.getType() != Material.ENDER_CHEST ||
                 arena.getStatus() != ArenaStatus.RUNNING ||
                 event.getAction() != Action.RIGHT_CLICK_BLOCK)
             return;
 
-        if (block.getType() == Material.ENDER_CHEST) {
-            for(Map.Entry<Inventory, Arena> entry : inventoryArenaHashMap.entrySet()){
+        final List<Inventory> inventories = inventoryArenaHashMap.get(arena);
 
-                final Inventory inventory = entry.getKey();
+        if (inventories == null)
+            return;
 
-                if(inventory.getHolder() == player){
-                    BedwarsAPI.getNMSHelper().simulateChestOpening(block);
-                    player.openInventory(inventory);
-                    openChests.put(player, block);
-                    break;
-                }
+        for (Inventory inventory : inventories) {
+            if (inventory.getHolder() == player) {
+                BedwarsAPI.getNMSHelper().simulateChestOpening(block);
+                player.openInventory(inventory);
+                openChests.put(player, block);
+                break;
             }
         }
     }
 
     @EventHandler
-    public void onInventoryClose(InventoryCloseEvent e){
+    public void onInventoryClose(InventoryCloseEvent e) {
         final Player player = (Player) e.getPlayer();
-        if(openChests.containsKey(player)){
+
+        if (openChests.containsKey(player)) {
             BedwarsAPI.getNMSHelper().simulateChestClosing(openChests.get(player));
             openChests.remove(player);
         }
