0d5eed6d1e9ba14c0f6b09735135a6e6177b1bac
MetallicGoat <ecazzam3@gmail.com>
2025-04-17 13:17:54 -0400

    Only remove invis if damage caused by non-team member

--- src/main/java/me/metallicgoat/tweaksaddon/tweaks/misc/RemoveInvisOnDamage.java
+++ src/main/java/me/metallicgoat/tweaksaddon/tweaks/misc/RemoveInvisOnDamage.java
@@ -1,13 +1,19 @@
 package me.metallicgoat.tweaksaddon.tweaks.misc;
 
 import de.marcely.bedwars.api.BedwarsAPI;
+import de.marcely.bedwars.api.GameAPI;
 import de.marcely.bedwars.api.arena.Arena;
+import de.marcely.bedwars.api.arena.Team;
+import java.util.UUID;
 import me.metallicgoat.tweaksaddon.config.MainConfig;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffectType;
 
@@ -22,8 +28,31 @@ public class RemoveInvisOnDamage implements Listener {
       final Player player = (Player) event.getEntity();
       final Arena arena = BedwarsAPI.getGameAPI().getArenaByPlayer(player);
 
-      if (arena != null && MainConfig.remove_invis_damge_causes.contains(event.getCause()))
-        player.removePotionEffect(PotionEffectType.INVISIBILITY);
+      // Is the damage what we be looking for???
+      if (arena != null && MainConfig.remove_invis_damge_causes.contains(event.getCause())) {
+        boolean damagedCausedByTeam = false;
+
+        // Is the damage from us or a team member???
+        if (event instanceof EntityDamageByEntityEvent) {
+          final EntityDamageByEntityEvent damageByEntityEvent = (EntityDamageByEntityEvent) event;
+          final Entity damager = damageByEntityEvent.getDamager();
+
+          // Get the owner of the fireball/tnt/gaurddog etc
+          final UUID uuid = GameAPI.get().getCompanionOwner(damager);
+          final Player damagerOwner = uuid != null ? Bukkit.getPlayer(uuid) : null;
+          final Arena damagerArena = damagerOwner != null ? BedwarsAPI.getGameAPI().getArenaByPlayer(damagerOwner) : null;
+          final Team damagerTeam = damagerArena != null ? damagerArena.getPlayerTeam(damagerOwner) : null;
+
+          if (arena.getPlayerTeam(player) == damagerTeam) {
+            damagedCausedByTeam = true;
+          }
+        }
+
+        // Only remove if caused by non-team member
+        if (!damagedCausedByTeam) {
+          player.removePotionEffect(PotionEffectType.INVISIBILITY);
+        }
+      }
     }
   }
 }
