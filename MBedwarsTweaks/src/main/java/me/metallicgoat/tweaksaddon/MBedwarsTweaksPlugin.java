c063a4187a315ab13ee8e23002c0081ca0f3dfe3
Ash <reyveeeee@gmail.com>
2026-09-22 09:49:57 +0500

    aa

--- MBedwarsTweaks/src/main/java/me/metallicgoat/tweaksaddon/MBedwarsTweaksPlugin.java
+++ MBedwarsTweaks/src/main/java/me/metallicgoat/tweaksaddon/MBedwarsTweaksPlugin.java
@@ -20,8 +20,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class MBedwarsTweaksPlugin extends JavaPlugin {
 
-  public static final int MIN_MBEDWARS_API_VER = 208;
-  public static final String MIN_MBEDWARS_VER_NAME = "5.5.8";
+  public static final int MIN_MBEDWARS_API_VER = 207;
+  public static final String MIN_MBEDWARS_VER_NAME = "5.5.7";
 
   @Getter
   private static MBedwarsTweaksPlugin instance;
